e663d470f07629e8f18c52f8dbcb6e6407a5cfaa
Karolina Krzęcio <karolina.krzecio@gmail.com>
2022-05-20 13:17:40 +0200

    Add

--- Palindrom.py
+++ Palindrom.py
@@ -1,9 +1,25 @@
 def is_palindrome(text):
-    text = text.lower().replace(" ", "")
-    if text == text[::-1]:
-     return True
-    return False
-print("Podaj słowo:")
-word = input()
 
-print(is_palindrome(word))
+    """Funkcja sprawdzająca czy dany wyraz/zdanie jest palindromem.
+     Funkcja usuwa ze sprawdzanego tekstu znaki interpunkcyjne i spacje."""
+
+    signs = [",",".",":",";","!","?"," ","-","..."]
+    for i in signs: 
+        text = ("".join(text.split()))
+        text = text.lower().replace(i, "")
+        if text == text[::-1]:
+            return True
+        return False
+
+# kilka testów:
+print(is_palindrome("Wakacje"))
+print(is_palindrome("KajaK"))
+print(is_palindrome("Popija rum As, Samuraj i Pop"))
+print(is_palindrome("Marzena pokazała Zakopane z ram"))
+print(is_palindrome("Kobyła ma mały Bok"))
+print(is_palindrome("Wół utył i ma miły tułów"))
+
+print()
+
+text = (input("Podaj własne słowo/zdanie: "))
+print(is_palindrome(text))
